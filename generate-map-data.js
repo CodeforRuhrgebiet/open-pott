2ed1f2ea2d8596471e5d79a35d1d993de48d3b80
mrzmyr <m0ritz.m3yer@gmail.com>
2016-07-15 20:29:37 +0200

    Add necessary files

---
+++ generate-map-data.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const data = require('./data/raw-data.json');
+
+const allowedCities = [
+  'breckerfeld',
+  'ennepetal',
+  'gevelsberg',
+  'herdecke',
+  'schwelm',
+  'sprockhövel',
+  'wetter (ruhr)',
+  'witten',
+  'werne',
+  'unna',
+  'selm',
+  'schwerte',
+  'lünen',
+  'kamen',
+  'holzwickede',
+  'fröndenberg/ruhr',
+  'bönen',
+  'bergkamen',
+  'waltrop',
+  'recklinghausen',
+  'oer-erkenschwick',
+  'marl',
+  'herten',
+  'haltern am see',
+  'gladbeck',
+  'dorsten',
+  'datteln',
+  'castrop-rauxel',
+  'hattingen',
+  'alpen',
+  'dinslaken',
+  'hamminkeln',
+  'xanten',
+  'wesel',
+  'voerde (niederrhein)',
+  'sonsbeck',
+  'schermbeck',
+  'rheinberg',
+  'neukirchen-vluyn',
+  'moers',
+  'kamp-lintfort',
+  'hünxe',
+  'oberhausen',
+  'mülheim an der ruhr',
+  'herne',
+  'hamm',
+  'hagen',
+  'bochum',
+  'bottrop',
+  'dortmund',
+  'duisburg',
+  'essen',
+  'gelsenkirchen'
+];
+
+data.features = data.features.filter((city) => {
+  let lowerCaseCity = city.properties.GN.toLowerCase();
+  let includesCity = allowedCities.indexOf(lowerCaseCity) > -1;
+
+  return includesCity;
+})
+
+let newJson = JSON.stringify(data, null, 4);
+
+fs.writeFileSync('./data/map-data.json', newJson);
